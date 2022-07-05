a8807ae2901c73ec946332319b97b65148dc72a2
React2 <apurv.dolphinwebsolution.com>
2022-07-05 18:07:31 +0530

    sorting change

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,16 +1,17 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Row, Spinner } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Spinner } from "react-bootstrap";
 import Footer from "./Footer";
 import Header from "./Header";
 import CustomPagination from "./CustomPagination";
 import Sorting from "./Sorting";
+import ProductData from "./ProductData";
 
 export default function Product() {
   const [data, setData] = useState([]);
-  const [order, setOrder] = useState([]);
+  const [, setOrder] = useState([]);
   const [searchInput, setSearchInput] = useState([]);
-  const [sorting, setSorting] = useState(false);
+  const [, setSorting] = useState(false);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const PER_PAGE = 10;
@@ -45,67 +46,12 @@ export default function Product() {
     <>
       <Header Data={data} setData={setData} searchInput={searchInput} />
       <div style={{ marginTop: "50px" }}>
-        <Sorting
-          Data={currentPosts}
-          setOrder={setOrder}
-          setSorting={setSorting}
-        />
+        <Sorting Data={data} setOrder={setOrder} setSorting={setSorting} />
       </div>
       {loading ? (
         <>
           <Container>
-            <Row className="my-5" lg={4} xs={1}>
-              {sorting ? (
-                <>
-                  {order.map((newdata) => {
-                    return (
-                      <Col className="col-md-3 my-2" key={newdata.id}>
-                        <Card height="400px" className="ap4">
-                          <Card.Img
-                            variant="top"
-                            height="200px"
-                            style={{ objectFit: "cover" }}
-                            src={newdata.image}
-                          />
-                          <Card.Body>
-                            <h6> Name: {newdata.name} </h6>
-                            <h6> Category: {newdata.category} </h6>
-                            <h6> Price: {newdata.price} </h6>
-                            <Link to={`/product/${newdata.slug}`}>
-                              <Button variant="primary">View Detail</Button>
-                            </Link>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    );
-                  })}
-                </>
-              ) : (
-                <>
-                  {currentPosts.map((newdata) => {
-                    return (
-                      <Col className="col-md-3 my-2" key={newdata.id}>
-                        <Card height="400px" className="ap4">
-                          <Card.Img
-                            variant="top"
-                            height="200px"
-                            src={newdata.image}
-                          />
-                          <Card.Body>
-                            <h6> Name: {newdata.name} </h6>
-                            <h6> Category: {newdata.category} </h6>
-                            <h6> Price: {newdata.price} </h6>
-                            <Link to={`/product/${newdata.slug}`}>
-                              <Button variant="primary">View Detail</Button>
-                            </Link>
-                          </Card.Body>
-                        </Card>
-                      </Col>
-                    );
-                  })}
-                </>
-              )}
-            </Row>
+            <ProductData Data={(data, currentPosts)} />
           </Container>
         </>
       ) : (
